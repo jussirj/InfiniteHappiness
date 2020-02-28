1c4bcbef7d50f18d0c152de75962ad5b410e1ecf
Eero Pitkänen <eero.pitkanen@reaktor.com>
2020-02-28 20:48:56 +0200

    Add cub animation

---
+++ Assets/Cub.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cub : MonoBehaviour
+{
+    Movin cub;
+
+    void Start()
+    {
+
+        cub = new Movin(transform, "json/cub");
+        Debug.Log(cub);
+        cub.Play();
+    }
+
+    void Update()
+    {
+
+    }
+
+}
