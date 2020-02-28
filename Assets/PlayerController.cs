29f5b7f47c6d098f214bfbb3d337ef077abab3a1
Jussi Rinta-Jaskari <jussi.rinta-jaskari@reaktor.com>
2020-02-28 20:24:50 +0200

    Player jump

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,16 +6,28 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 1f;
     private Transform cameraTransform;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
       this.cameraTransform = GameObject.Find("Main Camera").transform;
+      this.rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-          transform.position = new Vector3(
+        if (this.cameraTransform.position.y - transform.position.y > 3)
+        {
+            transform.position = new Vector3(transform.position.x, this.cameraTransform.position.y - 3, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.rb.AddForce(Vector3.up * 0.2f, ForceMode.Impulse);
+        }
+
+            transform.position = new Vector3(
           transform.position.x,
           transform.position.y,
           cameraTransform.position.z
