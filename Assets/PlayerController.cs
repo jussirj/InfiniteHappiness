bf5bfc361dfc72694f3bc6b15f56f4f2b168f220
Arttu Hanska <arttu.hanska@gmail.com>
2020-02-28 20:15:05 +0200

    Add player

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+    private Transform cameraTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+      this.cameraTransform = GameObject.Find("Main Camera").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          transform.position = new Vector3(
+          transform.position.x,
+          transform.position.y,
+          cameraTransform.position.z
+        );
+
+
+    }
+
+
+}
+
