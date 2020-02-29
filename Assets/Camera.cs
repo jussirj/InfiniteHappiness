68993002504cde31213378b347c33499b7a759e8
Jussi Rinta-Jaskari <jussi.rinta-jaskari@reaktor.com>
2020-02-29 12:59:40 +0200

    Camera script cleanup

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -10,12 +10,9 @@ public class Camera : MonoBehaviour
 
     private float zSpeed = 0.05f;
 
-    private Transform PlayerStopperTransform;
-
     // Start is called before the first frame update
     void Start()
     {
-        this.PlayerStopperTransform = GameObject.Find("PlayerStopper").transform;
     }
 
     // Update is called once per frame
@@ -25,28 +22,11 @@ public class Camera : MonoBehaviour
         {
             this.happiness += this.happinessStep;
             print("HAPPINESS: " + this.happiness);
-            /*
-            this.PlayerStopperTransform.localPosition = new Vector3(
-                this.PlayerStopperTransform.localPosition.x,
-                -5.5f,
-                this.PlayerStopperTransform.localPosition.z
-            );
-            */
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             this.happiness -= this.happinessStep;
             print("HAPPINESS: " + this.happiness);
-            /*
-            if (this.PlayerStopperTransform.localPosition.y < -1.5f)
-            {
-                this.PlayerStopperTransform.localPosition = new Vector3(
-                this.PlayerStopperTransform.localPosition.x,
-                this.PlayerStopperTransform.localPosition.y + 0.07f,
-                this.PlayerStopperTransform.localPosition.z
-            );
-            }
-            */
         }
 
         transform.position = new Vector3(
